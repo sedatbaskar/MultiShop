cf0ed9cd3b386f7439ba91cbbbfc1fcfe53c66fb
Sedat Başkar <sedatbaskar@gmail.com>
2025-05-21 23:41:52 +0200

    Create Config.cs Json web Token

---
+++ IdentityServer/Controllers/RegisterController.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer.Dtos;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegisterController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+
+        public async Task <IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values=new ApplicationUser()
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname
+            };
+
+            var results = await _userManager.CreateAsync(values,userRegisterDto.Password);
+            if(results.Succeeded)
+            {
+                return Ok("Kullanıcı başarıyla eklendi");
+            }
+            else
+            {
+                return BadRequest(results.Errors); // Hataları detaylı gönder
+            }
+        }
+    }
+}
